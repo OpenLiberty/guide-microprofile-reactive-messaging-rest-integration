a9352bf00d29ce6cce515cc6a04c1e595d6a5dc0
Ankit Agrawal <ankit.agrawal@ibm.com@Ankits-MacBook-Pro.local>
2020-05-07 11:07:17 -0400

    add test cases

--- finish/system/src/test/java/it/io/openliberty/guides/system/SystemServiceIT.java
+++ finish/system/src/test/java/it/io/openliberty/guides/system/SystemServiceIT.java
@@ -12,31 +12,50 @@
 // end::copyright[]
 package it.io.openliberty.guides.system;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.io.IOException;
 import java.time.Duration;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.junit.jupiter.api.Test;
 import org.microshed.testing.SharedContainerConfig;
 import org.microshed.testing.jupiter.MicroShedTest;
 import org.microshed.testing.kafka.KafkaConsumerConfig;
+import org.microshed.testing.kafka.KafkaProducerConfig;
 
+import io.openliberty.guides.models.PropertyMessage;
+import io.openliberty.guides.models.PropertyMessage.PropertyMessageDeserializer;
 import io.openliberty.guides.models.SystemLoad;
 import io.openliberty.guides.models.SystemLoad.SystemLoadDeserializer;
 
 @MicroShedTest
 @SharedContainerConfig(AppContainerConfig.class)
 public class SystemServiceIT {
+    
+    private static final long POLL_TIMEOUT = 30 * 1000;
 
     @KafkaConsumerConfig(valueDeserializer = SystemLoadDeserializer.class,
             groupId = "system-load-status",
             topics = "systemLoadTopic",
             properties = ConsumerConfig.AUTO_OFFSET_RESET_CONFIG + "=earliest")
     public static KafkaConsumer<String, SystemLoad> consumer;
+    
+    @KafkaConsumerConfig(valueDeserializer = PropertyMessageDeserializer.class, 
+            groupId = "property-name", topics = "propertyMessageTopic", 
+            properties = ConsumerConfig.AUTO_OFFSET_RESET_CONFIG + "=earliest")
+        public static KafkaConsumer<String, PropertyMessage> propertyConsumer;
+    
+    @KafkaProducerConfig(valueSerializer = StringSerializer.class)
+    public static KafkaProducer<String, String> propertyProducer;
 
     @Test
     public void testCpuStatus() {
@@ -51,5 +70,31 @@ public class SystemServiceIT {
             assertNotNull(sl.loadAverage);
         }
         consumer.commitAsync();
+    }
+    
+    @Test
+    public void testPropertyMessage() throws IOException, InterruptedException {
+        propertyProducer.send(new ProducerRecord<String, String>("propertyNameTopic", "os.name"));
+        
+        int recordsProcessed = 0;
+        long startTime = System.currentTimeMillis();
+        long elapsedTime = 0;
+        while (recordsProcessed == 0 && elapsedTime < POLL_TIMEOUT) {
+            ConsumerRecords<String, PropertyMessage> records = propertyConsumer.poll(Duration.ofMillis(3000));
+            System.out.println("Polled " + records.count() + " records from Kafka:");
+            for (ConsumerRecord<String, PropertyMessage> record : records) {
+                PropertyMessage pm = record.value();
+                System.out.println(pm);
+                assertNotNull(pm.hostname);
+                assertEquals("os.name", pm.key);
+                assertNotNull(pm.value);
+                recordsProcessed++;
+            }
+            consumer.commitAsync();
+            if (recordsProcessed > 0)
+                break;
+            elapsedTime = System.currentTimeMillis() - startTime;
+        }
+        assertTrue(recordsProcessed > 0, "No records processed");
     }
 }
