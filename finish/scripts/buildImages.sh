e7827fc4c175cab71629937d74da8fd19e3fe307
Gilbert Kwan <gkwan@ca.ibm.com>
2020-12-07 14:05:33 -0500

    Update buildImages.sh

--- finish/scripts/buildImages.sh
+++ finish/scripts/buildImages.sh
@@ -5,7 +5,6 @@ docker pull bitnami/zookeeper:3
 docker pull bitnami/kafka:2
 
 echo Building images
-
 docker build -t system:1.0-SNAPSHOT system/. &
 docker build -t inventory:1.0-SNAPSHOT inventory/. &
 
