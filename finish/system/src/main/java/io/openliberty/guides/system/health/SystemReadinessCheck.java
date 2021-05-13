103c776b14a3355df4e01bd0b647a5274f577242
Rutav Shah <rutav.shah@ibm.com>
2021-05-13 16:29:41 -0400

    fixed linting errors in SystemReadinessCheck.java

--- finish/system/src/main/java/io/openliberty/guides/system/health/SystemReadinessCheck.java
+++ finish/system/src/main/java/io/openliberty/guides/system/health/SystemReadinessCheck.java
@@ -32,31 +32,27 @@ import org.eclipse.microprofile.health.Readiness;
 @ApplicationScoped
 public class SystemReadinessCheck implements HealthCheck {
 
-    private static Logger logger = Logger.getLogger(SystemReadinessCheck.class.getName());
-    
+    private static Logger logger = Logger.getLogger(
+        SystemReadinessCheck.class.getName());   
     @Inject
     @ConfigProperty(name = "mp.messaging.connector.liberty-kafka.bootstrap.servers")
     String kafkaServer;
-    
     @Override
     public HealthCheckResponse call() {
         boolean up = isReady();
         return HealthCheckResponse.named(
             this.getClass().getSimpleName()).status(up).build();
     }
-
     private boolean isReady() {
         AdminClient adminClient = createAdminClient();
         return checkIfBarConsumerGroupRegistered(adminClient);
-    }
-    
+    } 
     private AdminClient createAdminClient() {
         Properties connectionProperties = new Properties();
         connectionProperties.put("bootstrap.servers", kafkaServer);
         AdminClient adminClient = AdminClient.create(connectionProperties);
         return adminClient;
     }
-    
     private boolean checkIfBarConsumerGroupRegistered(AdminClient adminClient) {
         ListTopicsResult topics = adminClient.listTopics();
         KafkaFuture<Collection<TopicListing>> topicsFuture = topics.listings();
