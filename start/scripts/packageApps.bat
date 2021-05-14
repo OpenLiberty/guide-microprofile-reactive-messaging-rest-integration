e4108680bf8fdd47f0b668572e3c26574f376987
Austin Seto <austin.seto@ibm.com>
2021-05-14 10:39:18 -0400

    Update Maven options

--- start/scripts/packageApps.bat
+++ start/scripts/packageApps.bat
@@ -1,2 +1,8 @@
-call mvn -pl models clean install
-call mvn clean package
+call mvn -Dhttp.keepAlive=false ^
+         -Dmaven.wagon.http.pool=false ^
+         -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 ^
+         -pl models clean install
+call mvn -Dhttp.keepAlive=false ^
+         -Dmaven.wagon.http.pool=false ^
+         -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 ^
+         -q clean package
