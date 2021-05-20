87b148b7ccec5abe6bb0a99bf9622d5c76f6325e
Gilbert Kwan <gkwan@ca.ibm.com>
2021-05-20 09:28:03 -0400

    update versions

--- finish/inventory/src/main/java/io/openliberty/guides/inventory/health/InventoryReadinessCheck.java
+++ finish/inventory/src/main/java/io/openliberty/guides/inventory/health/InventoryReadinessCheck.java
@@ -31,42 +31,43 @@ import org.eclipse.microprofile.health.Readiness;
 @Readiness
 @ApplicationScoped
 public class InventoryReadinessCheck implements HealthCheck {
-    private static Logger logger = Logger.getLogger(
-        InventoryReadinessCheck.class.getName());
+
+    private static Logger logger = Logger.getLogger(InventoryReadinessCheck.class.getName());
+    
     @Inject
     @ConfigProperty(name = "mp.messaging.connector.liberty-kafka.bootstrap.servers")
     String kafkaServer;
+    
     @Inject
     @ConfigProperty(name = "mp.messaging.incoming.systemLoad.group.id")
     String groupId;
+    
     @Override
     public HealthCheckResponse call() {
         boolean up = isReady();
-        return HealthCheckResponse.named(
-            this.getClass().getSimpleName()).status(up).build();
+        return HealthCheckResponse.named(this.getClass().getSimpleName()).state(up).build();
     }
+
     private boolean isReady() {
         AdminClient adminClient = createAdminClient();
         return checkIfBarConsumerGroupRegistered(adminClient);
     }
+    
     private AdminClient createAdminClient() {
         Properties connectionProperties = new Properties();
         connectionProperties.put("bootstrap.servers", kafkaServer);
         AdminClient adminClient = AdminClient.create(connectionProperties);
         return adminClient;
     }
+    
     private boolean checkIfBarConsumerGroupRegistered(AdminClient adminClient) {
         ListConsumerGroupsResult groupsResult = adminClient.listConsumerGroups();
-        KafkaFuture<Collection<ConsumerGroupListing>> consumerGroupsFuture =
-        groupsResult.valid();
+        KafkaFuture<Collection<ConsumerGroupListing>> consumerGroupsFuture = groupsResult.valid();
         try {
-            Collection<ConsumerGroupListing> consumerGroups =
-            consumerGroupsFuture.get();
-            for (ConsumerGroupListing g : consumerGroups) {
+            Collection<ConsumerGroupListing> consumerGroups = consumerGroupsFuture.get();
+            for (ConsumerGroupListing g : consumerGroups)
                 logger.info("groupId: " + g.groupId());
-            }
-            return consumerGroups.stream().anyMatch(
-                group -> group.groupId().equals(groupId));
+            return consumerGroups.stream().anyMatch(group -> group.groupId().equals(groupId));
         } catch (Exception e) {
             return false;
         }
--- finish/system/src/main/java/io/openliberty/guides/system/health/SystemReadinessCheck.java
+++ finish/system/src/main/java/io/openliberty/guides/system/health/SystemReadinessCheck.java
@@ -32,35 +32,37 @@ import org.eclipse.microprofile.health.Readiness;
 @ApplicationScoped
 public class SystemReadinessCheck implements HealthCheck {
 
-    private static Logger logger = Logger.getLogger(
-        SystemReadinessCheck.class.getName());
+    private static Logger logger = Logger.getLogger(SystemReadinessCheck.class.getName());
+    
     @Inject
     @ConfigProperty(name = "mp.messaging.connector.liberty-kafka.bootstrap.servers")
     String kafkaServer;
+    
     @Override
     public HealthCheckResponse call() {
         boolean up = isReady();
-        return HealthCheckResponse.named(
-            this.getClass().getSimpleName()).status(up).build();
+        return HealthCheckResponse.named(this.getClass().getSimpleName()).state(up).build();
     }
+
     private boolean isReady() {
         AdminClient adminClient = createAdminClient();
         return checkIfBarConsumerGroupRegistered(adminClient);
     }
+    
     private AdminClient createAdminClient() {
         Properties connectionProperties = new Properties();
         connectionProperties.put("bootstrap.servers", kafkaServer);
         AdminClient adminClient = AdminClient.create(connectionProperties);
         return adminClient;
     }
+    
     private boolean checkIfBarConsumerGroupRegistered(AdminClient adminClient) {
         ListTopicsResult topics = adminClient.listTopics();
         KafkaFuture<Collection<TopicListing>> topicsFuture = topics.listings();
         try {
             Collection<TopicListing> topicList = topicsFuture.get();
-            for (TopicListing t : topicList) {
+            for (TopicListing t : topicList)
                 logger.info("topic: " + t.name());
-            }
             return true;
         } catch (Exception e) {
             return false;
