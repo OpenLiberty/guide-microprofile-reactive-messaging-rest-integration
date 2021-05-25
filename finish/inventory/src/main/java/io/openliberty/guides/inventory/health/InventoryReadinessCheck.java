6c317b2e43193f8d9e759d1ff27c38ba85c73605
Gilbert Kwan <gkwan@ca.ibm.com>
2021-05-25 17:08:08 -0400

    Update InventoryReadinessCheck.java

--- finish/inventory/src/main/java/io/openliberty/guides/inventory/health/InventoryReadinessCheck.java
+++ finish/inventory/src/main/java/io/openliberty/guides/inventory/health/InventoryReadinessCheck.java
@@ -46,7 +46,8 @@ public class InventoryReadinessCheck implements HealthCheck {
     @Override
     public HealthCheckResponse call() {
         boolean up = isReady();
-        return HealthCheckResponse.named(this.getClass().getSimpleName()).state(up).build();
+        return HealthCheckResponse.named(
+            this.getClass().getSimpleName()).state(up).build();
     }
 
     private boolean isReady() {
@@ -63,12 +64,15 @@ public class InventoryReadinessCheck implements HealthCheck {
 
     private boolean checkIfBarConsumerGroupRegistered(AdminClient adminClient) {
         ListConsumerGroupsResult groupsResult = adminClient.listConsumerGroups();
-        KafkaFuture<Collection<ConsumerGroupListing>> consumerGroupsFuture = groupsResult.valid();
+        KafkaFuture<Collection<ConsumerGroupListing>> consumerGroupsFuture =
+        groupsResult.valid();
         try {
             Collection<ConsumerGroupListing> consumerGroups = consumerGroupsFuture.get();
-            for (ConsumerGroupListing g : consumerGroups)
+            for (ConsumerGroupListing g : consumerGroups) {
                 logger.info("groupId: " + g.groupId());
-            return consumerGroups.stream().anyMatch(group -> group.groupId().equals(groupId));
+            }
+            return consumerGroups.stream().anyMatch(
+                group -> group.groupId().equals(groupId));
         } catch (Exception e) {
             return false;
         }
