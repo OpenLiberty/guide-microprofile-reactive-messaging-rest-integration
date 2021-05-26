0807199b095c74adbfc7a496d882cb8651578e56
Gilbert Kwan <gkwan@ca.ibm.com>
2021-05-26 14:31:43 -0400

    Version update (#60)
    * update versions
    
    * changed linter issues in .sh files
    
    * fixed linter issues in Inventory and System LivenessCheck files
    
    * changed LivenessCheck files
    
    * changed SystemReadinessCheck.java
    
    * changed SystemReadinessCheck.java
    
    * fixed linting errors in SystemReadinessCheck.java
    
    * fixed linting errors in SystemReadinessCheck.java
    
    * updated SystemReadinessCheck.java
    
    * updated SystemReadinessCheck.java
    
    * fixed linting issue in SystemReadinessCheck.java
    
    * fixed InventoryReadinessCheck.java
    
    * fixed InventoryReadinessCheck.java
    
    * fixed InventoryReadinessCheck.java
    
    * fixed InventoryReadinessCheck.java
    
    * Update Maven options
    
    * update versions
    
    * updated InventoryReadinessCheck.java
    
    * changed InventoryReadinessCheck.java
    
    * Update InventoryReadinessCheck.java
    
    * Update InventoryReadinessCheck.java
    
    * Update InventoryReadinessCheck.java
    
    * Update InventoryReadinessCheck.java
    
    * Update InventoryReadinessCheck.java
    
    * Update InventoryReadinessCheck.java
    
    * Update InventoryReadinessCheck.java
    
    Co-authored-by: Rutav Shah <rutav.shah@ibm.com>
    Co-authored-by: Austin Seto <austin.seto@ibm.com>

--- start/scripts/packageApps.sh
+++ start/scripts/packageApps.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-
-mvn -pl models clean install
-mvn clean package
+mvn -Dhttp.keepAlive=false \
+    -Dmaven.wagon.http.pool=false \
+    -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
+    -pl models clean install
+mvn -Dhttp.keepAlive=false \
+    -Dmaven.wagon.http.pool=false \
+    -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
+    -q clean package
