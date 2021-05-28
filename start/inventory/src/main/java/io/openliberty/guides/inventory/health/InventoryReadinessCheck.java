225c8443b30fed304766ad126f2f3d5a7ff42a4d
Gilbert Kwan <gkwan@ca.ibm.com>
2021-05-28 10:40:30 -0400

    Version update (#60) (#63)
    * update versions
    
    * changed linter issues in .sh files
    
    * fixed linter issues in Inventory and System LivenessCheck files
    
    * changed LivenessCheck files
    
    * changed SystemReadinessCheck.java
    
    * changed SystemReadinessCheck.java
    
    * fixed linting errors in SystemReadinessCheck.java
    
    * fixed linting errors in SystemReadinessCheck.java
    
    * updated SystemReadinessCheck.java
    
    * updated SystemReadinessCheck.java
    
    * fixed linting issue in SystemReadinessCheck.java
    
    * fixed InventoryReadinessCheck.java
    
    * fixed InventoryReadinessCheck.java
    
    * fixed InventoryReadinessCheck.java
    
    * fixed InventoryReadinessCheck.java
    
    * Update Maven options
    
    * update versions
    
    * updated InventoryReadinessCheck.java
    
    * changed InventoryReadinessCheck.java
    
    * Update InventoryReadinessCheck.java
    
    * Update InventoryReadinessCheck.java
    
    * Update InventoryReadinessCheck.java
    
    * Update InventoryReadinessCheck.java
    
    * Update InventoryReadinessCheck.java
    
    * Update InventoryReadinessCheck.java
    
    * Update InventoryReadinessCheck.java
    
    Co-authored-by: Rutav Shah <rutav.shah@ibm.com>
    Co-authored-by: Austin Seto <austin.seto@ibm.com>
    
    Co-authored-by: Rutav Shah <rutav.shah@ibm.com>
    Co-authored-by: Austin Seto <austin.seto@ibm.com>

--- start/inventory/src/main/java/io/openliberty/guides/inventory/health/InventoryReadinessCheck.java
+++ start/inventory/src/main/java/io/openliberty/guides/inventory/health/InventoryReadinessCheck.java
@@ -1,6 +1,6 @@
 // tag::copyright[]
 /*******************************************************************************
- * Copyright (c) 2020 IBM Corporation and others.
+ * Copyright (c) 2020, 2021 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -32,42 +32,47 @@ import org.eclipse.microprofile.health.Readiness;
 @ApplicationScoped
 public class InventoryReadinessCheck implements HealthCheck {
 
-    private static Logger logger = Logger.getLogger(InventoryReadinessCheck.class.getName());
-    
+    private static Logger logger = Logger.getLogger(
+                                     InventoryReadinessCheck.class.getName());
+
     @Inject
     @ConfigProperty(name = "mp.messaging.connector.liberty-kafka.bootstrap.servers")
     String kafkaServer;
-    
+
     @Inject
     @ConfigProperty(name = "mp.messaging.incoming.systemLoad.group.id")
     String groupId;
-    
+
     @Override
     public HealthCheckResponse call() {
         boolean up = isReady();
-        return HealthCheckResponse.named(this.getClass().getSimpleName()).state(up).build();
+        return HealthCheckResponse.named(
+            this.getClass().getSimpleName()).state(up).build();
     }
 
     private boolean isReady() {
         AdminClient adminClient = createAdminClient();
         return checkIfBarConsumerGroupRegistered(adminClient);
     }
-    
+
     private AdminClient createAdminClient() {
         Properties connectionProperties = new Properties();
         connectionProperties.put("bootstrap.servers", kafkaServer);
         AdminClient adminClient = AdminClient.create(connectionProperties);
         return adminClient;
     }
-    
-    private boolean checkIfBarConsumerGroupRegistered(AdminClient adminClient) {
+
+    private boolean checkIfBarConsumerGroupRegistered(final AdminClient adminClient) {
         ListConsumerGroupsResult groupsResult = adminClient.listConsumerGroups();
-        KafkaFuture<Collection<ConsumerGroupListing>> consumerGroupsFuture = groupsResult.valid();
+        KafkaFuture<Collection<ConsumerGroupListing>> consumerGroupsFuture =
+            groupsResult.valid();
         try {
-            Collection<ConsumerGroupListing> consumerGroups = consumerGroupsFuture.get();
-            for (ConsumerGroupListing g : consumerGroups)
-                logger.info("groupId: " + g.groupId());
-            return consumerGroups.stream().anyMatch(group -> group.groupId().equals(groupId));
+          Collection<ConsumerGroupListing> consumerGroups = consumerGroupsFuture.get();
+          for (ConsumerGroupListing g : consumerGroups) {
+              logger.info("groupId: " + g.groupId());
+          }
+          return consumerGroups.stream().anyMatch(
+              group -> group.groupId().equals(groupId));
         } catch (Exception e) {
             return false;
         }
