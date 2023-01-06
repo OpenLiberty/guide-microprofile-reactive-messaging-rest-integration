9a482d0feaca0f357c00b0a9e810c622273b98f1
Jakub Pomykala <jakub.pomykala@hotmail.co.uk>
2023-01-06 15:24:26 +0000

    Adding -ntp to mvn in packageApps.sh

--- finish/scripts/packageApps.sh
+++ finish/scripts/packageApps.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-mvn -Dhttp.keepAlive=false \
+mvn -ntp -Dhttp.keepAlive=false \
     -Dmaven.wagon.http.pool=false \
     -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
     -pl models clean install
-mvn -Dhttp.keepAlive=false \
+mvn -ntp -Dhttp.keepAlive=false \
     -Dmaven.wagon.http.pool=false \
     -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
     -q clean package
