ea7ee7076c96034a68aae1e6aa3f904cb8663c1d
Gilbert Kwan <gkwan@ca.ibm.com>
2024-04-04 12:33:39 -0400

    Update testApp.sh

--- scripts/testApp.sh
+++ scripts/testApp.sh
@@ -6,8 +6,7 @@ set -euxo pipefail
 mvn -ntp -pl system verify
 mvn -ntp -pl inventory verify
 
-docker pull -q bitnami/zookeeper:3
-docker pull -q bitnami/kafka:2
+docker pull -q "bitnami/kafka:latest"
 
 ./scripts/buildImages.sh
 ./scripts/startContainers.sh
