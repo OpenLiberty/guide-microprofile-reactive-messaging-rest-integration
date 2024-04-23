5e9e7f5108e3a04b1e290784883ffe917cd5ae19
Gilbert Kwan <gkwan@ca.ibm.com>
2024-04-23 14:15:02 -0400

    Update dailyBuild

--- scripts/dailyBuild.sh
+++ scripts/dailyBuild.sh
@@ -5,7 +5,6 @@ do
         t) DATE="${OPTARG}";;
         d) DRIVER="${OPTARG}";;
         b) BUILD="${OPTARG}";;
-        u) DOCKER_USERNAME="${OPTARG}";;
         *) echo "Invalid option";;
     esac
 done
@@ -14,11 +13,5 @@ echo "Testing daily build image"
 
 sed -i "\#</containerRunOpts>#a<install><runtimeUrl>https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/$DATE/$DRIVER</runtimeUrl></install>" inventory/pom.xml system/pom.xml
 cat inventory/pom.xml system/pom.xml
-
-sed -i "s;FROM icr.io/appcafe/open-liberty:kernel-slim-java11-openj9-ubi;FROM $DOCKER_USERNAME/olguides:$BUILD;g" system/Dockerfile inventory/Dockerfile
-sed -i "s;RUN features.sh;#RUN features.sh;g" inventory/Dockerfile system/Dockerfile
-cat inventory/Dockerfile system/Dockerfile
-
-docker pull -q "$DOCKER_USERNAME""/olguides:""$BUILD"
 
 ../scripts/testApp.sh
